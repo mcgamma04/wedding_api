eeb445c076feeb9b6923b8d98b023f3043a87e3c
Adebayo Michael <mcgamma04@yahoo.com>
2022-09-08 16:47:28 +0100

    cors added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import connect from "./db/index.js";
 import UserRouter from "./routes/users.js";
@@ -6,6 +7,7 @@ dotenv.config();
 connect();
 const app = express();
 app.use(express.json());
+app.use(cors({ origin:process.env.CORS_ORIGIN}))
 app.use("/users", UserRouter);
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
